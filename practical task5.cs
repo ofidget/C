832e5ea13aedb3a154f6d24bd6dfc35790012ea0
Olga <olgazineva@mail.ru>
2022-12-20 23:38:52 +0300

    решена вторая задача

--- practical task5.cs
+++ practical task5.cs
@@ -3,13 +3,49 @@
 // // Напишите программу, которая покажет количество чётных чисел в массиве.
 // // [345, 897, 568, 234] -> 2
 
-Console.WriteLine("Программа поиска количества четных чисел в случайно заполненном трехзначными положительными числами  массиве");
+// Console.WriteLine("Программа поиска количества четных чисел в случайно заполненном трехзначными положительными числами  массиве");
+// void FillArray( int[] arr)
+// {
+//    Random random = new Random(); 
+//     for(int i=0; i<arr.GetLength(0); i++)
+//     {
+//             arr[i]= random.Next(100,999);
+//     }
+// } 
+//  void PrintArray( int[] arr)
+// {
+ 
+//     for(int i=0; i<arr.GetLength(0); i++)
+    
+//      {Console.Write(arr[i] + " ");
+//      }   
+       
+// }
+// int arrayCount = 7;
+// int [] array = new int[arrayCount];
+// int Count = 0;
+// FillArray(array);
+// PrintArray(array);
+// Console.WriteLine();
+//  for(int i=0; i < array.GetLength(0); i++)
+//     {
+//             if (array[i] % 2 == 0)
+//             Count++;
+//     }
+// Console.WriteLine("количество четных чисел в представленном массиве " + Count);
+// // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// //  [3, 7, 23, 12] -> 19
+
+// // [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Программа поиска суммы элементов, стоящих  на нечетных позициях в случайно заполненном массиве");
 void FillArray( int[] arr)
 {
    Random random = new Random(); 
     for(int i=0; i<arr.GetLength(0); i++)
     {
-            arr[i]= random.Next(100,999);
+            arr[i]= random.Next(-100,999);
     }
 } 
  void PrintArray( int[] arr)
@@ -23,21 +59,15 @@ void FillArray( int[] arr)
 }
 int arrayCount = 7;
 int [] array = new int[arrayCount];
-int Count = 0;
+int Sum = 0;
 FillArray(array);
 PrintArray(array);
 Console.WriteLine();
- for(int i=0; i < array.GetLength(0); i++)
+ for(int i=0; i < array.GetLength(0); i=i+2)
     {
-            if (array[i] % 2 == 0)
-            Count++;
+      Sum = array[i] + Sum;
     }
-Console.WriteLine("количество четных чисел в представленном массиве " + Count);
-// // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
-
-// //  [3, 7, 23, 12] -> 19
-
-// // [-4, -6, 89, 6] -> 0
+Console.WriteLine("сумма элементов, стоящих на нечетных позициях " + Sum); //с точки зрения пользователя, когда первый элемент соответствует индексу i=0
 
 // // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементом массива.
 
